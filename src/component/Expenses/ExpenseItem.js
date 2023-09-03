b9d7978f9c5b16308663b6ffde8b3a4bb8b9e0fd
shad-bin-abi-aydid <shadaydid@gmail.com>
2023-09-03 03:21:49 +0100

    New component ExpenseFilter is added to show expenses graphically (Yearly)

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,26 +1,17 @@
-import React,{ useState } from 'react';
+import React from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) =>{
 
-const [title, setTitle] = useState(props.title);
-
-
-const clickHandling = () => {
-  setTitle('Updated!');
-  console.log(title);
-  };
-    
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h1>{title}</h1>
-        <div className="expense-item__price">{props.amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandling}>Click Me</button>
     </Card>
   );
 }
